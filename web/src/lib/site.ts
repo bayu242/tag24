83ec68f83695805e54752b717a67a2b424f052f5
Bayu Seto Aji <bayusetoaji354@gmail.com>
2026-09-20 23:14:41 +0700

    fix apk url

--- web/src/lib/site.ts
+++ web/src/lib/site.ts
@@ -1,6 +1,6 @@
 export const REPO_URL = "https://github.com/bayu242/tag24";
 export const APK_URL =
-  "https://github.com/bayu242/tag24/releases/download/v1.0.0/Tag24-v1.0.0.0.apk";
+  "https://github.com/bayu242/tag24/releases/download/v1.0.1/Tag24-v1.0.0.1.apk";
 export const NFC_TOOLS_URL =
   "https://play.google.com/store/apps/details?id=com.wakdev.wdnfc";
 
